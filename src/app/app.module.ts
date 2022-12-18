4e0ff0012f2c8765fb6ca4ac7e5032238641a9b5
PTOPatrick <patrick.toma@gmx.ch>
2022-12-18 17:38:23 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment as env } from 'src/environments/environment';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent,
+    NavComponent,
+    LogoutButtonComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: env.auth.domain,
+      clientId: env.auth.clientId
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import auth from '../../auth_config.json';
 
 export const environment = {
-  production: false
+  production: false,
+  auth: {
+    ...auth,
+    redirectUri: window.location.origin
+  }
 };
 
 /*
