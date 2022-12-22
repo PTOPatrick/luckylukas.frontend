e88b98566751171b1b30a124626a10c1b212b68b
PTOPatrick <PTOPatrick@users.noreply.github.com>
2022-12-22 15:51:01 +0000

    deploy: 83429bae243a9773dbf18e2af647376806407f46

--- src/app/models/steam.account.ts
+++
@@ -1,8 +0,0 @@
-export interface SteamAccount {
-    Name: string;
-    CsRank: string;
-    Owner: string;
-    Created: Date;
-    Changed: Date;
-    IsBanned: boolean;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import auth from '../../auth_config.json';
-
-export const environment = {
-  production: true,
-  auth: {
-    ...auth,
-    redirectUri: window.location.origin
-  }
-};
